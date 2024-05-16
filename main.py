109fa1e79d9988cc62513c9d0111398de5c0389f
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-05-16 12:36:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+# Požádání uživatele o vstup
+vstupni_text = input("Zadejte řetězce oddělené čárkami: ")
+retezce = vstupni_text.split(',')
+
+# Vytvoření defaultdict pro kategorizaci řetězců
+kategorie = defaultdict(list)
+# Zde dokonči úlohu svým kódem...
