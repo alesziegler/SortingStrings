88eb079b37c8b8d3b2bb2d9f7a6b113d0f5b2ae1
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-05-17 20:17:33 +0000

    Final

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from collections import defaultdict
 
-# Požádání uživatele o vstup
 vstupni_text = input("Zadejte řetězce oddělené čárkami: ")
-retezce = vstupni_text.split(',')
+retezce = vstupni_text.split(', ')
 
 # Vytvoření defaultdict pro kategorizaci řetězců
 kategorie = defaultdict(list)
@@ -21,21 +20,20 @@ for word in retezce:
   except ValueError:
     first_character = word[0]
     kategorie[first_character].append(word)
-
-sorted_kategorie = sorted(kategorie)
-
-print(kategorie)
-
-print(sorted_kategorie)
-
-def list_items(list):
-  for item in list:
-    print(item,end=",")
-    
-
+   
 for character in sorted(kategorie):
   printable_words = ""
-  for word in kategorie.get(character):
+  count = 1
+  x = kategorie.get(character)
+  
+  for word in x:
     printable_words += word
-  print(f"{character} : {kategorie.get(character)}")
-  print(printable_words)
+    
+    if count < len(x):
+      printable_words += ", "
+
+    count += 1
+      
+  print(f"{character}: {printable_words}")
+
+
