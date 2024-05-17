d5514e169cbef5662f3aa8e3d6c7b3cc97788ad4
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-05-17 09:20:40 +0000

    Working core established.

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ retezce = vstupni_text.split(',')
 # Vytvoření defaultdict pro kategorizaci řetězců
 kategorie = defaultdict(list)
 # Zde dokonči úlohu svým kódem...
+
+#ok, in retezce, we have a list of strings.
+#now we need to make keys out of the first character of every string
+#what if we have two strings with the same first sign?
+#well then the second string will be the second item on a list, 
+#which is a value of a dictionary.
+
+for word in retezce:
+  try:
+    int(word[0])
+    kategorie["čísla"].append(word)
+  except ValueError:
+    first_character = word[0]
+    kategorie[first_character].append(word)
+
+print(kategorie)
